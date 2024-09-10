cc92d1ba22ba23824d8131f36dd999622347d4ec
cgarroni <122840792+CGarroni@users.noreply.github.com>
2024-09-10 10:11:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,58 @@
+let athletes = [
+  {
+    titulo: "Daniel Dias: O Rei da Natação Paralímpica",
+    descricao: "Daniel Dias é um nadador paralímpico brasileiro, considerado um dos maiores atletas paralímpicos da história. Ele possui diversas medalhas em Jogos Paralímpicos e recordes mundiais.",
+    link: "https://pt.wikipedia.org/wiki/wikipedia_link",
+    modalidade: "Natação",
+    apelido: "Dani Dias",
+    idade: 37,
+    tags: "nadador natação atleta brasileiro ",
+    medalhas: {
+        ouro: 14,
+        prata: 7,
+        bronze: 6
+    }
+},
+  {
+    titulo: "André Brasil",
+    descricao: "André Brasil é um nadador paralímpico brasileiro, conhecido por sua velocidade e resistência. Ele possui diversas medalhas em Jogos Paralímpicos e recordes mundiais.",
+    link: "https://pt.wikipedia.org/wiki/André_Brasil",
+      apelido: "",
+      modalidade: "Natação",
+      idade: 40,
+      tags: "nadador natação atleta brasileiro ",
+      medalhas: { 
+        ouro: 7, 
+        prata: 5,
+        bronze: 2
+      }
+  },
+  {
+    titulo: "Clodoaldo Silva",
+    descricao: "Clodoaldo Silva é um nadador paralímpico brasileiro, considerado um dos maiores atletas paralímpicos da história. Ele possui diversas medalhas em Jogos Paralímpicos e recordes mundiais.",
+    link: "https://pt.wikipedia.org/wiki/Clodoaldo_Silva",
+      apelido: "Tubarão Paralímpico",
+      modalidade: "Natação",
+      idade: 45,
+      tags: "nadador natação atleta brasileiro ",
+      medalhas: { 
+        ouro: 6, 
+        prata: 6,
+        bronze: 2
+      }
+  },
+  {
+    titulo: "Ádria Santos",
+    descricao: "Ádria Santos é uma atleta paralímpica brasileira, especializada em provas de velocidade. Ela possui diversas medalhas em Jogos Paralímpicos e recordes mundiais.",
+    link: "https://pt.wikipedia.org/wiki/%C3%81dria_Santos",
+      apelido: "Filha do vento",
+      modalidade: "Atletismo",
+      tags: "atletismo velocidade rápida atleta brasileira ",
+      idade: 50,
+      medalhas: { 
+        ouro: 4, 
+        prata: 8,
+        bronze: 1
+      }
+  }
+];
---
+++ index.js
@@ -0,0 +1,49 @@
+function search() {
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("results-search");
+
+  let searchField = document.getElementById("input-search").value
+
+  // se o searchField for uma string sem nada
+  if (!searchField) {
+    section.innerHTML = "<p>Não foi encontrado. Você precisa digitar o nome de um atleta ou esporte...</p>"
+    return
+  }
+
+  searchField = searchField.toLowerCase()
+
+  // Inicializa a string que armazenará os resultados
+  let results = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada atleta no array `athletes`
+  for (let data of athletes) {
+    titulo = data.titulo.toLocaleLowerCase()
+    descricao = data.descricao.toLocaleLowerCase()
+    tags = data.tags.toLocaleLowerCase()
+    
+    //se titulo includes searchField
+    if (titulo.includes(searchField) || descricao.includes(searchField) || tags.includes(searchField)) {
+      // cria um novo elemento
+      results += `
+      <div class="result-item">
+          <h2>
+            <a href="#" target="_blank">${data.titulo}</a>
+          </h2>
+          <p class="descrition-athlete">${data.descricao}</p>
+          <a href=${data.link}>Mais informações</a>
+        </div>
+    `;
+    }
+  }
+
+  if (!results){
+    results = <p>"Nada foi encontrado"</p>
+  }
+
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = results;
+}
+
